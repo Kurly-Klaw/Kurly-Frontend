b5331673c5f1c0223e4a28159b6eccfacbc308ae
Gbariel <gabriel.leitao2015@gmail.com>
2025-02-03 16:59:56 -0300

    Create routers for backend integration with axios and add package.json

---
+++ routes/RegisterRoutes.js
@@ -0,0 +1,99 @@
+import api from '../services/api'
+
+
+export function createRegister(payload, user_id) {
+    const headers = {
+        headers: {
+            user_id: user_id
+        }
+    };
+
+    api.post('/register', payload, headers)
+        .then(function (response) {
+            if (response.status === 201) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
+
+export function getRegister(date) {
+
+    const params = {
+        params:{
+            date: date
+        }
+    };
+
+    api.get('/register', params)
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
+
+export function getRegisterById(register_id) {
+
+    const registerUrl = `/register/${register_id}`
+
+    api.get(registerUrl)
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
+
+export function updateRegister(payload, register_id) {
+
+    const registerUrl = `/register/${register_id}`
+
+    api.put(registerUrl, payload)
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
+
+export function updateRegisterStatus(payload, register_id) {
+
+    const registerUrl = `/register/${register_id}/status`;
+
+    api.put(registerUrl, payload)
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
+
+export function deleteRegister(register_id) {
+
+    const registerUrl = `/register/${register_id}`;
+
+    api.delete(registerUrl)
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
---
+++ routes/ScheduleRoutes.js
@@ -0,0 +1,70 @@
+import api from '../services/api'
+
+
+export function createSchedule(payload) {
+    api.post('/schedule', payload)
+        .then(function (response) {
+            if (response.status === 201) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
+
+export function getSchedule(start_date, end_date) {
+    const headers = {
+        headers: {
+            start_date: start_date,
+            end_date: end_date
+        }
+    };
+
+    api.get('/schedule', headers)
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
+
+export function updateSchedule(payload, start_date, end_date) {
+    const headers = {
+        headers: {
+            start_date: start_date,
+            end_date: end_date
+        }
+    };
+
+    api.put('/schedule', payload, headers)
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
+
+export function deleteRegister(date) {
+    const params = {
+        params: {
+            date: date
+        }
+    };
+
+    api.delete('/schedule', params)
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
